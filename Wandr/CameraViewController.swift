c9cafbbffc7cdec5d94d93bbee5dc1efc4df2ceb
jellybeanjohnny <mamerige@icloud.com>
2016-07-19 10:54:13 -0400

    Added tabBar as the root, and some skeleton classes to act as the viewcontroller tabs

---
+++ Wandr/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  Wandr
+//
+//  Created by Matt Amerige on 7/19/16.
+//  Copyright © 2016 David Park. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.redColor()
+        title = "Camera"
+    }
+}
---
+++ Wandr/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  Wandr
+//
+//  Created by Matt Amerige on 7/19/16.
+//  Copyright © 2016 David Park. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blueColor()
+        title = "Feed"
+    }
+}
