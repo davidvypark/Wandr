e73a710c8742df2e00aead195bdeb5ba90cb4ce3
jellybeanjohnny <mamerige@icloud.com>
2016-07-19 11:37:57 -0400

    changed camera background color to green

--- Wandr/CameraViewController.swift
+++ Wandr/CameraViewController.swift
@@ -12,7 +12,7 @@ class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.redColor()
+        view.backgroundColor = UIColor.greenColor()
         title = "Camera"
     }
 }
